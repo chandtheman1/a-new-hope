8191d7467202974cfd32bb192c4650d27eb33b7c
Chandler Liang <chandler.liang123@gmail.com>
2021-10-17 20:58:46 +1100

    created the nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+// import { Router, RouterModule, Routes } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    // RouterModule.forRoot(appRoutes, { enableTracing: true})
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
